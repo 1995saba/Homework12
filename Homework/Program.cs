36d5bdb3515efdae6ef543b8ee80ed68a42c268e
Sabina <sabina.orunbaeva@gmail.com>
2017-09-07 10:53:37 +0600

    Add files via upload

---
+++ Homework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type consoleType = typeof(Console);
+
+            foreach(var method in consoleType.GetMethods())
+            {
+                Console.WriteLine(method.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User { Id = 123, Login = "admin", Password = "123qwerty" };
+            Type userType = typeof(User);
+
+            foreach(var property in userType.GetProperties())
+            {
+                Console.WriteLine(property.ToString() + "  =  " +property.GetValue(user));
+            }
+            Console.ReadLine();
+        }
+    }
+}
